baa19e740e0f51df35a3b2a98ad826bc75f608ea
ArtemNovorai <artnovorai@gmail.com>
2018-04-24 20:43:34 +0300

    commit #6' git commit -m commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var myCrazyCat = {
+"name": "A ridiculous object",
+"someArray": [7, 9, {purpose: "confution", number: 888 }, 3.3],
+"random animal": "Banana shark"
+};
+myCrazyCat.someArray[2].number
+
+888
