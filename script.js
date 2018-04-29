a37b75096537d2756471c17be86cb12aafe8631b
ArtemNovorai <artnovorai@gmail.com>
2018-04-30 00:40:18 +0300

    commit NEW .js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-$("#main").fadeOut(3000);
+var time = function () {
+  prompt("Time Out Boys!")
+}
+setTimeout(time, 3000);
+
+var r = 1;
+var print = function () {
+  console.log("Это просто потрясно" +  r  + "тик-так");
+  r++;
+  
+};
+var interval = setInterval(print, 1000);
