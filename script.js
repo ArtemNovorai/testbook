27ff429b52cdcfd484e049ef8e5114c49daf5544
ArtemNovorai <artnovorai@gmail.com>
2018-04-30 00:08:16 +0300

    commit NEW .js file

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-var myCrazyCat = {
-"name": "A ridiculous object",
-"someArray": [7, 9, {purpose: "confution", number: 888 }, 3.3],
-"random animal": "Banana shark"
-};
-myCrazyCat.someArray[2].number
-
-888
+$("#main").fadeOut(3000);
