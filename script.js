203a91825720210d6b3a442351539628be7338a0
ArtemNovorai <artnovorai@gmail.com>
2018-05-03 00:54:16 +0300

    I dont know

--- script.js
+++ script.js
@@ -12,30 +12,17 @@
 //var interval = setInterval(print, 1000);
 
 
-var Random = function (size) {
+var getRandomNumber = function (size) {
   return Math.floor(Math.random()* size);
 };
 
-var width = 800;
-var height = 600;
-var clicks = 0;
-
-var target = {
-  x: Random(width);
-  y: Random(height);
+var getDistance = function (event, target) {
+  var diffX = event.offsetX - target.x;
+  var diffY = event.offsetY - target.y;
+  return Math.sqrt((diffX * diffX) + (diffY * diffY));
 };
 
-$("#map").click(function (event) {
-  clicks++;
-  var distance = DistanceHit (event, target);
-  var DST = Distance(distance);
-  $("#distance").text(DST);
-  if (distance < 7) {
-    alert(" You found the GOLD in " + clicks + " clicks! ")
-  }
-});
-
-var Distance = function (distance) {
+var getDistanceHint = function (distance) {
   if (distance < 10) {
     return "Umbelible hot!! =)";
   } else if (distance < 20) {
@@ -49,13 +36,31 @@ var Distance = function (distance) {
   } else {
     return "Freeeeeez this police!!";
   }
-}
-
-var DistanceHit = function (event, target) {
-  var diffX = event.offsetX - target.x;
-  var diffY = event.offsetY - target.y;
-  return Math.sqrt((diffX * diffX) + (diffY * diffY))
 };
+
+
+var width = 800;
+var height = 600;
+var clicks = 0;
+
+var target = {
+  x: getRandomNumber(width),
+  y: getRandomNumber(height)
+};
+
+$("#map").click(function (event) {
+  clicks++;
+  var distance = getDistance (event, target);
+  var distanceHint = getDistanceHint(distance);
+  $("#distance").text(distanceHint);
+  if (distance < 7) {
+    alert(" You found the GOLD in " + clicks + " clicks! ")
+  }
+});
+
+
+
+
 
 
 
